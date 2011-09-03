1e34c3fed2474e83aa13df1a80f7ebd68d13b0ae
David Kethel <davidkethel@gmail.com>
2011-09-03 22:04:33 +1000

    Added some test cases and code

--- overLapFourSpecs.js
+++ overLapFourSpecs.js
@@ -1,10 +1,104 @@
 describe("BringToFront", function() {
 
 	it("should be able to do this", function() {
-		expect(bringToFront([ "red", "green", "blue" ], "red"))
-				.toEqual("green");
+		expect(bringToFront([ "red" ], "red")).toEqual([ "red" ]);
 
 	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "green" ], "green")).toEqual([ "green" ]);
+
+	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "blue" ], "blue")).toEqual([ "blue" ]);
+	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow" ], "yellow")).toEqual([ "yellow" ]);
+	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow", "red" ], "red")).toEqual(
+				[ "yellow", "red" ]);
+	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow", "green" ], "green")).toEqual(
+				[ "yellow", "green" ]);
+	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow", "blue" ], "blue")).toEqual(
+				[ "yellow", "blue" ]);
+	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "red", "yellow" ], "yellow")).toEqual(
+				[ "red", "yellow" ]);
+	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow", "red", ], "yellow")).toEqual(
+				[ "red", "yellow" ]);
+	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow", "blue", ], "yellow")).toEqual(
+				[ "blue", "yellow" ]);
+	});
+
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow", "green", ], "yellow")).toEqual(
+				[ "green", "yellow" ]);
+	});
+
+	
+	
+	
+	
+	
+	it("should be able to do this", function() {
+		expect(bringToFront([  "blue", "red","yellow" ], "yellow")).toEqual(
+				[ "blue", "red", "yellow" ]);
+	});
+	
+	it("should be able to do this", function() {
+		expect(bringToFront([  "blue", "green","yellow" ], "yellow")).toEqual(
+				[ "blue", "green", "yellow" ]);
+	});
+	
+	it("should be able to do this", function() {
+		expect(bringToFront([  "green", "red","yellow" ], "yellow")).toEqual(
+				[ "green", "red", "yellow" ]);
+	});
+	
+	
+	
+	
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow", "blue", "red", ], "yellow")).toEqual(
+				[ "blue", "red", "yellow" ]);
+	});
+	
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow", "blue", "green", ], "yellow")).toEqual(
+				[ "blue", "green", "yellow" ]);
+	});
+	
+	it("should be able to do this", function() {
+		expect(bringToFront([ "yellow", "green", "red", ], "yellow")).toEqual(
+				[ "green", "red", "yellow" ]);
+	});
+	
+	
+	
+	it("should be able to do this", function() {
+		expect(bringToFront([  "green","yellow", "red", ], "yellow")).toEqual(
+				[ "green", "red", "yellow" ]);
+	});
+	
+	
 
 });
 
@@ -27,9 +121,8 @@ describe("BringToFront", function() {
  * player.play(song); player.pause(); });
  * 
  * it("should indicate that the song is currently paused", function() {
- * expect(player.isPlaying).toBeFalsy();
- *  // demonstrates use of 'not' with a custom matcher
- * expect(player).not.toBePlaying(song); });
+ * expect(player.isPlaying).toBeFalsy(); // demonstrates use of 'not' with a
+ * custom matcher expect(player).not.toBePlaying(song); });
  * 
  * 
  * });
--- overlapFour.js
+++ overlapFour.js
@@ -1,4 +1,37 @@
-function bringToFront(colours , colour  ){
-	
-	return colours[2];
+function bringToFront(colours, colour) {
+
+	if (colours.length == 3) {
+		if (colours[0] == colour) {
+
+			var newArray = [];
+
+			newArray[0] = colours[1];
+			newArray[1] = colours[2];
+			newArray[2] = colours[0];
+			return newArray;
+		}
+
+		if (colours[1] == colour) {
+
+			var newArray = [];
+			newArray[0] = colours[0];
+			newArray[1] = colours[2];
+			newArray[2] = colours[1];
+
+			return newArray;
+		}
+	}
+
+	if (colours.length == 2) {
+		if (colours[1] != colour) {
+
+			var newArray = [];
+
+			newArray[0] = colours[1];
+			newArray[1] = colours[0];
+			return newArray;
+		}
+	}
+
+	return colours;
 }
